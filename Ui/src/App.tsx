3854c3391b85ae987abd7d97a32c06c8a75816c5
Hendra Dwi Saputra <depadeh@hotmail.com>
2025-01-22 19:52:13 +0700

    add Tailwind CSS and Bootstrap Icons integration with custom styles

--- Ui/src/App.tsx
+++ Ui/src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <>
-      <div>
-        <p>Halu Dunia</p>
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-xl font-bold tracking-wider">
+          <i className="bi bi-globe-asia-australia me-5"></i>
+          <span>Sphinx of black quartz, judge my vow</span>
+        </p>
       </div>
     </>
   )
